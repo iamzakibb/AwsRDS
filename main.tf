7c0bfc0d033d7d4387e3b888309db0ac69490c39
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-12-25 12:53:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+module "rds" {
+  source               = "./modules/rds"
+  db_name              = var.db_name
+  instance_identifier  = var.instance_identifier
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  allocated_storage    = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  backup_retention     = var.backup_retention
+  preferred_backup_window = var.preferred_backup_window
+  multi_az             = var.multi_az
+  monitoring_interval  = var.monitoring_interval
+  performance_insights = var.performance_insights
+  deletion_protection  = var.deletion_protection
+  skip_final_snapshot  = var.skip_final_snapshot
+  admin_username       = var.admin_username
+  admin_password       = var.admin_password
+  vpc_security_group_ids = var.vpc_security_group_ids
+  db_subnet_group_name = var.db_subnet_group_name
+}
